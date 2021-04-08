b32ad41ae4a55d3ae3542d9c9978c8db1faf797e
lkl <lhzcm888888@gmail.com>
2021-04-08 18:00:23 +0800

    添加了获取图片信息字段名称的方法

--- src/utility/common.js
+++ src/utility/common.js
@@ -1,5 +1,18 @@
 import md5 from "./md5.js"
 
+const imgExifInfo = {
+    "Make": "设备厂商",
+    "Model": "型号",
+    "ExposureTime": "曝光时间",
+    "FNumber": "光圈值",
+    "ISOSpeedRatings": "感光度",
+    "ApertureValue": "镜头光圈",
+    "FocalLength": "焦距",
+    "ShutterSpeedValue": "快门速度",
+    "LensMake": "镜头厂商",
+    "LensModel": "镜头型号",
+}
+
 const common = {
     //md5转换
     md5Convert: function(obj){
@@ -15,6 +28,10 @@ const common = {
             target[parseInt(i/nums)].push(array[i])
         }
         return target
+    },
+    //获取图片信息字段
+    getImgExifFiel: function(fiel){
+        return imgExifInfo[fiel]
     }
 }
 
--- src/views/Dynamic.vue
+++ src/views/Dynamic.vue
@@ -1,3 +1,15 @@
 <template>
     <div><h1>动态页面开发中....</h1></div>
 </template>
+
+
+<script>
+import request from "../utility/request.js"
+export default {
+    created:function(){
+        request.get("/publish/1/10","",function(res){
+            console.log(res)
+        })
+    }
+}
+</script>
